377413a7c66562f8cceebc81088de1e81580605c
Rajat <rajat.khanna_cs18@gla.ac.in>
2021-10-31 19:21:37 +0530

    Solid Rectangle Pattern Code

---
+++ src/Star_Patterns/SolidRectangle/SolidRectangle.java
@@ -0,0 +1,19 @@
+package Star_Patterns.SolidRectangle;
+import java.util.*;
+
+public class SolidRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
